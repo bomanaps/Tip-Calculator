5967dc5934bc34934091d1c819145aa462020805
bomanaps <mercy.boma35@yahoo.com>
2022-06-19 15:30:23 -0700

    My tip calculator task commit

---
+++ script.js
@@ -0,0 +1,18 @@
+et calculate = document.querySelector("#calculate");//can also use getElementById
+
+calculate.addEventListener("click", () => {
+	let totalBill = document.querySelector("#totalBill").value;
+	let tipPercent = document.querySelector("#tipPercent").value;
+	let split = document.querySelector("#split").value;
+
+	//validate
+	if (totalBill === "" || tipPercent == 0 || split == 0) {
+		alert("Please enter values");
+		return;
+	}
+	//calculate
+	let total = (totalBill * tipPercent) / split;
+	total = total.toFixed(2);
+
+	document.getElementById("tip").innerHTML = total;
+});
